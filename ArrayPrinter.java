9c187db3bba6de458c4710ac2479cf974670a0e4
mvandenbroek@ION.LOCAL <mvandenbroek@ION.LOCAL>
2020-01-22 11:19:49 +0100

    Versie 1

---
+++ ArrayPrinter.java
@@ -0,0 +1,11 @@
+package Toetsversiebeheer;
+
+public class ArrayPrinter {
+	public static void main(String[] args) {
+		 double a[][] = { { Math.random(), Math.random(), Math.random(), Math.random(),Math.random() }};
+		 printarray(a);
+}
+	public void printarray (double[][] a) {
+		
+	}
+}
